25e89be1f1ee1adab16b85fc3531ce3c39ed0bb7
Bruno <Bruno@172.16.195.113>
2015-06-16 14:31:09 -0300

    Fiz novamente a função com o Algortimo de Newton

---
+++ JS/AlgoritmoNewton.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function Derivada1()
+{
+    
+}
+
+function Derivada2()
+{
+    
+}
+
+function Funcao()
+{
+    
+}
+
+function Newton()
+{
+    
+    
+}
