4d8c8efa8df9ed6307e7d729eb8b15268c914881
Lorena <lorenacampos41@gmail.com>
2015-06-16 20:56:56 -0300

    Algoritmo Pégaso

--- JS/Pégaso.js
+++ JS/Pégaso.js
@@ -5,3 +5,33 @@
  */
 
 
+function Pegaso(a,b,Toler,IterMax){
+    var FA;
+    FA = f(a);
+    var FB;
+    var FX;
+    var Iter;
+    var DELTAX;
+    FB = f(b);
+    x=b; FX=FB; Iter =0;
+    do{
+        DELTAX = -FX/(FB-FA)*(b-a);
+        x=x+DELTAX;
+        FX=f(x);
+        document.write("Iter" + Iter + "a" + a +"FA"+ FA + "b" + b+ "FB" + FB + "X" + x+ "FX" + FX + "DELTA X" + DELTAX );
+        
+    }while((abs(DELTAX)>Toler && abs(FX)>Toler)||Iter < IterMax)
+    if(FX*FB <0){
+    
+    
+    
+    }
+    
+}
+
+function f(x){
+    
+    
+    
+    
+}
