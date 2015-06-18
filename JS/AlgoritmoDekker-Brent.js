998d4234125eaa15b229c55eb8cdb37512ee5552
Bruno <brunaofaraujo2005@hotmail.com>
2015-06-18 10:25:10 -0300

    Finalização do Algoritmo de Dekker-Brent

--- JS/AlgoritmoDekker-Brent.js
+++ JS/AlgoritmoDekker-Brent.js
@@ -65,8 +65,71 @@ function DekkerBrent(a,b, Toler, IterMax)
            // Interrompa !!! Escolha entre Interpolação e Bisseção
        }
        
+       if (abs(e) >= Tol || abs(Fa) > abs(Fb) )
+       {
+           var s = Fb/Fa;
+           if(a === c) // Interpolação Linear
+           {
+               var p = 2*z*s;
+               var q = 1-s;
+            }
+            else // Interpolação Inversa Quadrática
+            {
+                q   = Fa/Fc;
+            var r   = Fb/Fc;
+                p   = s*(2*z*q*(q-r)-(b-a)*(r-1));
+                q   = (q-1)*(r-1)*(s-1);
+            }
+            if(p>0)
+            {
+                q = -q;
+            }
+            else
+            {
+                p = -p;
+            }
+            if((2*p)< min(3*z*q-abs(Tol*q), abs(e*q) )) // Aceita Interpolação
+            {
+                e = d;
+                d = p/q;
+            }
+            else // usa bisseção devido a falha na interpolação
+            {
+                d = z;
+                e = z;
+            }
+        
+       }
+       else
+       {
+          d = z;
+          e = z; 
+       }
+       
+    a = b;
+    Fa = Fb;
+    
+    if(abs(d) > Tol)
+    {
+        b = b+d;
+    }
+    else
+    {
+        b = b+ sinal(z)* Tol;
+    }
+    Iter = Iter+1;
+    Fb   = Funcao(b); // Avaliar a função em b ;
    }while(a>0)
     
-    
-    
+    var Raiz = b;
+
+    if(abs(z) <= Tol || Fb === 0)
+    {
+        var CondErro = 0;
+    }
+    else
+    {
+        var CondErro = 1;
+    }
+        
 }
